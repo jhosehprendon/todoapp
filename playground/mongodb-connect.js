45c150947bc2a983e77cf56515ede14145075b72
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-27 08:30:05 -0500

    Add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if(err){
@@ -17,16 +18,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2))
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Jhosehp',
-        age: 30,
-        location: 'Bogota'
-    }, (err, result) => {
-        if(err) {
-            return console.log('Unable to insert user', err);
-        }
-        console.log(result.ops)
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Jhosehp',
+    //     age: 30,
+    //     location: 'Bogota'
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+    //     console.log(result.ops)
+    // });
 
     db.close();
 });
